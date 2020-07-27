eed26fbb9738a59c983ddd5ce2c341acc2fe3481
Jared <addairjared@gmail.com>
2020-07-26 17:19:48 -0700

    changed the connection to databse

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'Packers04!',
-      database : 'smartbrain'
-    }
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+      rejectUnauthorized: false,
+      },
+      },
   });
 
 
